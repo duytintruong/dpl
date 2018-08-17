1bcbc6704f55493a06c6cc0679314c5363049d22
Duy Tin Truong <duytin.truong@gmail>
2018-08-17 14:48:52 +1000

    set version 0.1.0

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='dpl',
     packages=['dpl'],
-    version='0.1',
+    version='0.1.0',
     description='A library for building readable and concise data pipelines',
     author='Duy Tin Truong',
     author_email='',
     url='https://github.com/duytintruong/dpl',
-    download_url='https://github.com/duytintruong/dpl/archive/0.1.tar.gz',
+    download_url='https://github.com/duytintruong/dpl/archive/0.1.0.tar.gz',
     keywords=['data', 'pipeline'],
     classifiers=[],
 )
